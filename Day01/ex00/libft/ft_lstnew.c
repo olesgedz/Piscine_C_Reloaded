5b4db17ec7ce05f2433bcf03d9706d57311e8c6c
Jeromy Black balaq <jblack-b@e3r7p3.21-school.ru>
2018-12-05 20:33:05 +0300

    eet

--- Day01/ex00/libft/ft_lstnew.c
+++ Day01/ex00/libft/ft_lstnew.c
@@ -6,33 +6,42 @@
 /*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 20:53:34 by jblack-b          #+#    #+#             */
-/*   Updated: 2018/12/05 17:10:28 by jblack-b         ###   ########.fr       */
+/*   Updated: 2018/12/05 20:31:25 by jblack-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 t_list		*ft_lstnew(void const *content, size_t content_size)
 {
 	t_list *new;
 
 	new = NULL;
+	printf("d21312wasdadf3\n\n\n");
 	if (!(new = (t_list *)(malloc(sizeof(t_list)))))
 		return (NULL);
-	if (new)
+	if (content == NULL)
 	{
-		if (content == NULL)
-		{
-			new->content = NULL;
-			new->content_size = 0;
-		}
-		else
-		{
-			new->content = (void *)content;
-			new->content_size = content_size;
-		}
-		new->next = NULL;
+		new->content = NULL;
+		new->content_size = 0;
+		printf("d213123");
 	}
+	else
+	{
+
+		new->content = malloc(content_size * sizeof(char));
+		printf("dasfsd");
+		if (!(new->content = malloc(content_size * sizeof(char))))
+		{
+			printf("dassadasdasddfg324234");
+			free(new);
+			return (NULL);
+		}
+		new->content = (void *)(content);
+		new->content_size = content_size;
+	}
+	new->next = NULL;
 	return (new);
 }
---
+++ Day01/ex00/libft/main.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblack-b <jblack-b@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/22 18:30:02 by jblack-b          #+#    #+#             */
+/*   Updated: 2018/12/05 20:31:57 by jblack-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+void		ft_memcpy_check(int n)
+{
+	char a[] = "segfaulter tu dois";
+	char b[0];
+	char c[0];
+	ft_memcpy(b, a, n);
+	memcpy(c, a, n);
+	printf("f:%s\nr:%s\n", b, c);
+}
+
+void		ft_memccpy_check()
+{
+	char src[] = "test basic du memccpy !";
+	char buff1[22];
+	char buff2[22];
+	printf("%c\n", *(char*)ft_memccpy(buff1, src, 'm', 22));
+	printf("%c\n", *(char*)memccpy(buff2, src, 'm', 22));
+	printf("f:%s\nr:%s\n", buff1, buff2);
+}
+
+void		ft_memcmp_check()
+{
+	char *s1 = "\xff\xaa\xde\x12";
+	char *s2 = "\xff\xaa\xde\x12MACOSAAAAA";
+	size_t size = 4;
+	int i1 = memcmp(s1, s2, size);
+	int i2 = ft_memcmp(s1, s2, size);
+	printf("r:%d\nf:%d", i1, i2);
+}
+
+void		ft_strlcat_check()
+{
+	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+	char buff1[0xF00] = "there is no stars in the sky";
+	char buff2[0xF00] = "there is no stars in the sky";
+	size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
+	size_t r1 = strlcat(buff1, str, max);
+	size_t r2 = ft_strlcat(buff2, str, max);
+
+	printf("r:%s %zu\nf:%s %zu\n ", buff1, r1, buff2, r2);
+}
+
+int		main()
+{
+	//ft_memcpy_check(17);
+	//ft_memccpy_check();
+	//ft_memcmp_check();
+	//ft_strlcat_check();
+//	printf("\n%d\n%d\n", ft_atoi("9223372036854775805"), atoi("9223372036854775805"));
+//	printf("%s\n", ft_strtrim("   A   "));
+//char** matrix = ft_strsplit("      split       this for   me  !       ", ' ');
+//int c = L'ø';
+//ft_putchar(c);
+//while(*matrix != 0)
+//printf("%s\n", *matrix++);
+t_list *boop;
+
+	boop = ft_lstnew("\nfuun\n", 0);
+
+	printf("%s", boop->content);
+	return (0);
+}
